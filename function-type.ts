98ef5bb1691395823207df62635f9953ebb84751
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-04-07 15:53:28 +0800

    ts

---
+++ function-type.ts
@@ -0,0 +1,23 @@
+// TS 中的函数类型
+function hello (){}
+
+const hello1 = function(){};
+
+const hello2 = () => {};
+
+// 本改写成这样 function add(first: number, second: number):number
+// 但是由于 TS 的类型推断功能，可以省略函数的返回值类型
+function add(first: number, second: number){
+	// 此时需要对 add 函数添加类型注解
+	return first + second + '';
+}
+
+const total2 = add(1, 2);
+
+
+// 函数的返回值类型为 void 类型
+function sayHello (): void{
+	console.log('hello');
+	// return ''; 
+}
+
