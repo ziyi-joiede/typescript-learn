ad8848b1748635cc83671dde7d119914b4bb0ae7
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-04-14 22:03:35 +0800

    基于 express 框架完善爬虫功能

---
+++ crowller/src/utils/crawller.ts
@@ -0,0 +1,42 @@
+// TS 直接引用 JS包会有问题
+// ts -> .d.ts 翻译文件 -> js
+import superagent from 'superagent';
+import fs from 'fs';
+import path from 'path';
+import DellAnalyzer from './analyzer';
+
+export interface Analyzer {
+	analyze:(html:string, filePath:string) => string
+}
+
+class Crawler {
+	private filePath = path.resolve(__dirname, '../../data/course.json');
+	
+	private async getRawHtml(){
+		let res = await superagent.get(this.url);
+		return res.text;
+	}
+
+	private writeFile(content:string){
+		fs.writeFileSync(this.filePath, content);
+	}
+
+	private async initSpiderProcess(){
+		// 获取 url 对应的 html 的内容
+		const html = await this.getRawHtml();
+		const fileContent = this.analyzer.analyze(html,this.filePath);
+		this.writeFile(fileContent);
+	}
+	
+	constructor(private url: string, private analyzer: Analyzer){
+		this.initSpiderProcess();
+	}
+
+}
+
+const secret = 'secretKey';
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+let analyzer = DellAnalyzer.getInstance(); 
+new Crawler(url, analyzer);
+
+export default Crawler
